9e457ba6db9a3e6484aeb9cafb1415f79efbc40f
BenSymons <bsymonsb@hotmail.co.uk>
2021-01-07 16:52:42 +0000

    deleted unneeded files and add logo component

---
+++ src/Components/logo.jsx
@@ -0,0 +1,8 @@
+import { logDOM } from "@testing-library/react"
+import React from "react"
+
+const logo = () => {
+    return (<img alt="green logo" src="https://green.cdn.energy/branding/logo-r.svg"></img>)
+}
+
+export default logo;
