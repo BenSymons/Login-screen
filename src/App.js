a93a1fde54f56f9e1e3dcfb70f24cfd46c994e57
BenSymons <bsymonsb@hotmail.co.uk>
2021-01-15 14:42:51 +0000

    add outer border with shadow and centered

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import Login from "./Components/login_form"
 
 function App() {
   return (
-    <div className="App">
-      <Logo/>
-      <h2>Operations studio</h2>
-      <p class="prompt">Please enter your email below</p>
-      <Login/>
+    <div className="container">
+      <div className="App">
+        <Logo/>
+        <h2>Operations studio</h2>
+        <p class="prompt">Please enter your email below</p>
+        <Login/>
+      </div>
     </div>
   );
 }
